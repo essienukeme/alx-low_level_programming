017fdb26dee90d2c9784d920be75aefd1d338453
Ukeme Essien <essien.ukeme@yahoo.com>
2022-09-08 13:25:02 -0700

    Cisfun

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#!/bin/ba/**
+	   * main - Entry point
+	   *
+	   * Return: Always 0 (Success)
+	   */
+int main(void)
+{
+	puts("\"Programming is like a building a multilingual puzzle");
+	return (0);
+}

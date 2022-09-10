9a2b4da9429656a20d50fd9cbf4e03738e51941e
Ukeme Essien <essien.ukeme@yahoo.com>
2022-09-09 17:20:27 -0700

    loops

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Print to screen
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n, ld;
+
+	srand(time(0));

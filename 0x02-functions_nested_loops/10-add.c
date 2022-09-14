b1bfd2944be96276eee31c8f311705707397ed11
Ukeme Essien <essien.ukeme@yahoo.com>
2022-09-13 17:40:12 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Sums of two integers
+ * @num1: This is an integer
+ * @num2: This is an integer
+ *
+ * Return: sum
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}

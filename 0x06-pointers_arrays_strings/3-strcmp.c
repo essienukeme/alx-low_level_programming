948f4e60008cca6261b40385327c2ac6cdd44adc
Ukeme Essien <essien.ukeme@yahoo.com>
2022-09-22 15:21:23 -0700

    a function that compares two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_strcmp- copies the pointed string
+ *@s1: check
+ *@s2:check
+ *
+ *Return: int
+ */
+
+int _strcmp(char *s1, char  *s2)
+{
+	int i, output;
+
+	i = 0;
+
+	while (*(s1 + i) == *(s2 + i) && *(s2 + i) && *(s1 + i) != '\0')
+		i++;
+	output = (*(s1 + i) - *(s2 + i));
+	return (output);
+}

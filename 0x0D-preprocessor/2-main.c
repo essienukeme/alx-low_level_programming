84b1cbe5153680074b462d79b162244cd49f8be8
Ukeme Essien <essien.ukeme@yahoo.com>
2022-10-10 02:10:44 -0700

    done

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the file that runs this code
+ *
+ * Return: Null Void
+ */
+int main(void)
+{
+	printf("%s\n", __BASE_FILE__);
+	return (0);
+}

2b25db604ef26bf5fa276a91a4a66ee336d2df75
Ukeme Essien <essien.ukeme@yahoo.com>
2022-10-12 18:33:40 -0700

    sum, difference, product, division, modulo

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - add two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - finds the difference between two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: diff between a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: quotient of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Finds the modulus of two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: modulus of a and b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

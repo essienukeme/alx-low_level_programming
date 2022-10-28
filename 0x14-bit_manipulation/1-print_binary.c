0a249ae47a1b6c30a376fe756e494337dbbbad8c
Ukeme Essien <essien.ukeme@yahoo.com>
2022-10-28 16:46:51 -0700

    a function that prints the binary representation of a number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_binary - function that prints the 
+ * print_binary - function that prints the
  * binary representation of a number
  * @n: number to be printed in binary
  */
